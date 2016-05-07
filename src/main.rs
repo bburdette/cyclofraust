0dfb6ebbf92a62f3680bdc197255b5e3cd981614
Ben B <bburdette@gmail.com>
2016-05-07 16:18:16 -0600

    alsa version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate libc;
-extern crate rsoundio;
 
 use std::ffi::CString;
 use std::net::UdpSocket;
@@ -7,22 +6,25 @@ use std::net::SocketAddr;
 use std::io::{Error,ErrorKind};
 use std::string::String;
 use std::fmt::format;
+use std::time::Duration;
 use std::sync::mpsc;
 use std::thread;
 use std::str::FromStr;
 use std::cmp::min;
-use std::env;
 
-// #[macro_use]
-// mod tryopt;
-// mod stringerror;
+extern crate alsa;
+use alsa::Direction;
+use alsa::ValueOr;
+use alsa::pcm::{PCM, HwParams, Format, Access};
+
+type Sample = f32;
 
 extern crate tinyosc;
 use tinyosc as osc;
 
 extern {
   pub fn fraust_init(samplerate: i32);
-  pub fn fraust_compute(count: i32, input: *mut libc::c_float, output: *mut libc::c_float );
+  pub fn fraust_compute(count: i32, input: *const libc::c_float, output: *mut libc::c_float );
   pub fn fraust_setval(label: *const libc::c_char , val: libc::c_float); 
 }
 
@@ -40,212 +42,194 @@ pub struct KeyEvt {
 , position: f32
 }
 
-// const CHANNELS: i32 = 2;
-// const NUM_SECONDS: i32 = 5;
-const SAMPLE_RATE: f64 = 48000.0;
-// const SAMPLE_RATE: f64 = 44100.0;
-// const FRAMES_PER_BUFFER: u32 = 64;
+const CHANNELS: i32 = 2;
+const NUM_SECONDS: i32 = 5;
+const SAMPLE_RATE: f64 = 44100.0;
+const FRAMES_PER_BUFFER: u32 = 64;
 // const FRAMES_PER_BUFFER: u32 = 2048;
 
 fn main() {
-  
-  let args = env::args();
-  let mut iter = args.skip(1); // skip the program name
-
-  let latency = 
-    match iter.next() {
-      Some(latency) => {
-        FromStr::from_str(&latency).unwrap()
-      }
-      None => {
-        0.006
-      }
-    };
-
-  run(latency);
-}
-
-/*
-fn main() {
-    match run() {
-      Err(e) => println!("error {:?}", e),
-      _ => (),
-    }
     run().unwrap()
 }
 
 
-fn run() -> Result<(), Box<std::error::Error> >
-*/
-fn run(latency: f64) 
-{
-    // make a channel to receive updates from the osc.
-    let (tx, rx) = mpsc::channel::<KeyEvt>();
+fn run() -> Result<(), Box<std::error::Error> > {
 
-    // we'll do osc receive below, in the main thread.
+
+    // ---------------------------------------------
+    // make a channel to receive updates from the osc.
+
+    let (tx, rx) = mpsc::channel::<KeyEvt>();
 
     // ---------------------------------------------
     // init fraust 
     // ---------------------------------------------
-    println!("initting fraust with sample rate: {}", SAMPLE_RATE);
+    println!("initting with sample rate: {}", SAMPLE_RATE);
 
     unsafe { fraust_init(SAMPLE_RATE as i32); }
 
     let bufmax = 10000;
     let mut inflts = [0.0;10000];
+    inflts[0] = 1.0;
 
-    let mut outflts: Vec<f32> = vec![0.0; 10000];
-    let mut frames = vec![outflts.clone(), outflts.clone()];
+    let mut outflts = [0.0;10000];
+    // let mut outflts = [0.0;bufmax];
 
     let volstring = CString::new("Volume").unwrap();
 
     unsafe { fraust_setval(volstring.as_ptr(), 0.05); }
 
-    let mut loopcount = 0;
-    let mut buflen = 0;
-    let bufmaxu = bufmax as usize;
-    let mut bufidx = bufmaxu - 1;
+    // let mut loopcount = 0;
+    // let mut buflen = 0;
+    // let bufmaxu = bufmax as usize;
+    // let mut bufidx = bufmaxu - 1;
+
+    // make a full buffer to begin with.
+    // unsafe { fraust_compute(bufmax, flts.as_mut_ptr(), outflts.as_mut_ptr()); }
 
     // ---------------------------------------------
-    // start the rsoundio process!
+    // init alsa 
     // ---------------------------------------------
 
-    // create an audio context
-    let mut sio = rsoundio::SoundIo::new();
-    sio.set_name("rsoundio-example").unwrap();
+    /*
+    let config = default_config();
+    let mut phases: Vec<Phase> = (*config.pitches).iter().map(|&p| phase(&config, p)).collect();
+    phases.sort();
+    let phases = phases;
 
-    // print the backends.
-    let becount = sio.backend_count();
+    let phase_min = phases[0];
+    let phase_max = phases[phases.len()-1];
+    let sample_rate = config.sample_rate;
+    let samples = phase_max * 2;
 
-    println!("backend count {}", becount);
+    let mut backing_vector: Vec<Sample> = Vec::with_capacity(samples);
+    // Should probably use Vec::from_elem(samples, 0) but that is not in stable yet
+    unsafe { backing_vector.set_len(samples); }
+    let mut data = &mut backing_vector[..];
+    */
 
-    for i in 0..becount {
-      let be = sio.backend(i);
-      println!("backend #{}: {:?} ", i, be);
+    // input, output buffers.
+    // let sample_count = 10000;
+    // let sample_count = 1000;
+    // let sample_count = 959;
+    let sample_count = 16;
+    // let sample_count = 900;
+    let sample_rate = 44100;
+    let mut input_vector: Vec<Sample> = Vec::with_capacity(sample_count);
+    // Should probably use Vec::from_elem(samples, 0) but that is not in stable yet
+    unsafe { input_vector.set_len(sample_count); }
+    let mut inputdata = &mut input_vector[..];
+
+    let mut output_vector: Vec<Sample> = Vec::with_capacity(sample_count);
+    // Should probably use Vec::from_elem(sample_count, 0) but that is not in stable yet
+    unsafe { output_vector.set_len(sample_count); }
+    let mut outputdata = &mut output_vector[..];
+
+     
+    let default = CString::new("default").unwrap();
+    let nonblock = false; 
+    /*
+    let pcm_in = PCM::open(&*default, Direction::Capture, nonblock).unwrap();
+    {
+      let hwp = HwParams::any(&pcm_in).unwrap();
+      hwp.set_channels(1).unwrap();
+      hwp.set_rate(sample_rate, ValueOr::Nearest).unwrap();
+      hwp.set_format(Format::float()).unwrap();
+      hwp.set_access(Access::RWInterleaved).unwrap();
+      pcm_in.hw_params(&hwp).unwrap();
     }
+    let io_in = pcm_in.io_f32().unwrap();
+    pcm_in.prepare().unwrap();
+    */
 
-    // let be = try_opt_resbox!(sio.backend(2), "backend error");
-    let be = sio.backend(1).unwrap();
-    sio.connect_backend(be).unwrap();
+    let pcm_out = PCM::open(&*default, Direction::Playback, nonblock).unwrap();
+    {
+      let hwp = HwParams::any(&pcm_out).unwrap();
+      hwp.set_rate_near(sample_rate, ValueOr::Nearest).unwrap();
+      hwp.set_buffer_size_near(1024).unwrap();
+      hwp.set_period_size_near(64,ValueOr::Nearest).unwrap();
+      hwp.set_channels(2).unwrap();
+      hwp.set_format(Format::float()).unwrap();
+      hwp.set_access(Access::RWInterleaved).unwrap();
+      pcm_out.hw_params(&hwp).unwrap();
+    }
+    let io_out = pcm_out.io_f32().unwrap();
+    pcm_out.prepare().unwrap();
 
-    // connect to the default audio backend
-    // sio.connect().unwrap();
-    println!("Connected to backend: {}", sio.current_backend().unwrap());
-    sio.flush_events();
-    // get default output device
-    let dev = sio.default_output_device().unwrap();
-    assert!(dev.probe_error().is_none());
-    println!("Using output device: {}", dev);
+    match pcm_out.hw_params_current() {
+      Ok(params) => println!("hwparams: {:?}", params),
+      _ => println!("failed to get params"),
+    }
+   
+    // println!("fraust compute!"); 
+    // unsafe { fraust_compute(sample_count as i32, inflts.as_ptr(), outflts.as_mut_ptr()); }
 
-    // create output stream
-    let mut out = dev.create_outstream().unwrap();
-    assert!(out.set_name("noise").is_ok());
-    out.set_format(rsoundio::SioFormat::Float32LE).unwrap();
-    println!("setting latency: {:?}", latency);
-    out.set_latency(latency);
-    println!("Output format: {}", out.format().unwrap());
-
-    // ----------------------------------
-    // register callbacks
-    // ----------------------------------
-    out.register_write_callback(|out: rsoundio::OutStream,
-                                 min_frame_count: u32,
-                                 max_frame_count: u32| {
-        
-        // println!("latency: {:?}", out.latency());
-
-        // any events to update the DSP with?? 
-        match rx.try_recv() { 
-          Ok(ke) => {
-            match ke.evttype { 
-              KeType::KeyHit => { 
-                  // println!("setting vol to 0.3!");
-                  unsafe { fraust_setval(volstring.as_ptr(), 0.3); }
-                }
-              KeType::KeyUnpress => { 
-                  // println!("setting vol to 0.001!");
-                  unsafe { fraust_setval(volstring.as_ptr(), 0.001); }
-                }
-              _ => {}
-            }
-          }
-          _ => {}
-        }
-
-        // do dsp!
-
-        // compute samples.
-
-        let comp_count = min(max_frame_count, outflts.len() as u32);
-
-        // println!("callback, min {}, max {}, comp_count {}", min_frame_count, max_frame_count, comp_count);
-
-        unsafe { fraust_compute(comp_count as i32, inflts.as_mut_ptr(), frames[0].as_mut_slice().as_mut_ptr()); }
-
-        // println!("meh: {}, {}, {}", outflts[0], min_frame_count, max_frame_count);
-
-        // let chan = vec![&outflts];
-        // let frames = vec![vec!&outflts, &outflts];
-        // let frames = vec![&outflts,&outflts];
-        // let frames = vec![outflts,outflts];
-        // let r = outflts.clone();
-        // let frames = vec![outflts.clone(), outflts.clone()];
-
-        // frames[1].clone_from_slice(frames[0].as_slice());
-        // frames[1].clone_from(&frames[0]);
-        {
-          let (l,r) = frames.split_at_mut(1);
-          r[0].clone_from(&l[0]);
-        }
-
-
-        // out.write_stream_f32(min_frame_count, &frames).unwrap();
-        out.write_stream_f32(comp_count, &frames).unwrap();
-
-        // out.clear_buffer();
-
-        /*
-        let l: Vec<f32> = samples.iter()
-                                 .cycle()
-                                 .take(max_frame_count as usize + pos)
-                                 .skip(pos)
-                                 .map(|s| *s)
-                                 .collect();
-        pos = (max_frame_count as usize + pos) % cycle_len;
-        let r = l.clone();
-        let frames = vec![l, r];
-        out.write_stream_f32(min_frame_count, &frames).unwrap();
-        */
-    });
-    out.register_underflow_callback(|out: rsoundio::OutStream| {
-        println!("Underflow in {} occured!", out.name().unwrap())
-    });
-    out.register_error_callback(|out: rsoundio::OutStream, err: rsoundio::SioError| {
-        println!("{} error: {}", out.name().unwrap(), err)
-    });
-
-    // open output stream
-    out.open().unwrap();
-    let sr = out.sample_rate();
-    println!("output sample rate: {}", sr);
-
-    let layout = out.layout();
-    println!("Output channel layout: {}", layout);
-    // start audio output (now the `write_callback` will be called periodically)
-    out.start().unwrap();
+    // copy vals into output array.
+    let mut idx = 0;
+    for _ in 0..sample_count {
+        outputdata[idx] = outflts[idx];
+        idx += 1;
+    }
+    
+    //println!("instate: {:?}", pcm_in.state());
+    println!("outstate: {:?}", pcm_out.state());
 
     let oscrecvip = std::net::SocketAddr::from_str("0.0.0.0:8000").expect("Invalid IP");
-    // since sound is in a separate thread, 
-    // use this thread to do the osc receiver stuff.
-    match oscthread(oscrecvip, tx) {
-      Ok(s) => println!("oscthread exited ok"),
-      Err(e) => println!("oscthread error: {} ", e),
-    };
+    // spawn the osc receiver thread. 
+    thread::spawn(move || {
+      match oscthread(oscrecvip, tx) {
+        Ok(s) => println!("oscthread exited ok"),
+        Err(e) => println!("oscthread error: {} ", e),
+      }
+    });
+
+    let mut iters = 0;
+
+    loop {
+      // let samps = try!(io_in.readi(&mut inputdata));
+      // try!(io_in.readi(&mut inputdata));
+      // println!("instate: {:?}", pcm_in.state());
+      // println!("outstate: {:?}", pcm_out.state());
+
+      // any events to update the DSP with?? 
+      match rx.try_recv() { 
+        Ok(ke) => {
+          println!("message");
+          match ke.evttype { 
+            KeType::KeyHit => { 
+                println!("setting vol to 0.3!");
+                unsafe { fraust_setval(volstring.as_ptr(), 0.3); }
+              }
+            KeType::KeyUnpress => { 
+                println!("setting vol to 0.001!");
+                unsafe { fraust_setval(volstring.as_ptr(), 0.001); }
+              }
+            _ => {}
+          }
+        }
+        _ => {}
+      }
+
+      iters = iters + 1;
+      // println!("fraust compute! {}", iters); 
+      unsafe { fraust_compute(sample_count as i32, inflts.as_ptr(), outflts.as_mut_ptr()); }
+      let mut idx = 0;
+      for _ in 0..sample_count {
+          outputdata[idx] = outflts[idx];
+          idx += 1;
+      }
+      // println!("copied {} samples", idx);
+      
+      // std::thread::sleep(Duration::from_millis(10));
+      try!(io_out.writei(outputdata));
+    
+    }
 
     println!("its over!");
 
-    // Ok(())
+    Ok(())
 }
+
 
 fn oscthread(oscrecvip: SocketAddr, sender: mpsc::Sender<KeyEvt>) -> Result<String, Error> { 
   let socket = try!(UdpSocket::bind(oscrecvip));
